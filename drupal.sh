8bfbc63d39b81655a4931c215b76f1534de886d5
saeedhmohd244 <saeedhmohd244@gmail.com>
2024-10-29 17:02:41 +0530

    Update drupal.sh

--- drupal.sh
+++ drupal.sh
@@ -2,7 +2,6 @@
 
 # Update and upgrade the system
 sudo apt update
-sudo apt upgrade -y
 
 # Install NGINX, MySQL, PHP, and necessary PHP extensions
 sudo apt install -y nginx mysql-server php8.3-cli php8.3-fpm php8.3-mysql php8.3-xml php8.3-gd php8.3-curl php8.3-mbstring php8.3-zip php8.3-intl
